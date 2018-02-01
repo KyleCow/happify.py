dc0b285a9f4d254d28e0ac7f07f4cc0d64b9b743
KyleCow <9131@kingdown.wilts.sch.uk>
2018-02-01 21:32:13 +0000

    Update happify.py

--- happify.py
+++ happify.py
@@ -1,1 +1,9 @@
+number = str(input('What number would you like to \'Hapify\': '))
+number = list(number)
 
+happy = 0
+
+for i in range(len(number)):
+    happy = happy + (int(number[i])*int(number[i]))
+
+print(happy)
